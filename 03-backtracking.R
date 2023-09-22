667dd56f7f38bd0205fb16cb5be1652d31dc5472
Fiqah-Alihan <20b2126@ubd.edu.bn>
2023-09-22 23:09:20 +0800

    Fiqah3-(Updated5)

--- 03-backtracking.R
+++ 03-backtracking.R
@@ -26,33 +26,54 @@ backtracks_df <- collatz_df %>%
 print(backtracks_df)
 
 
-# 2) Most frequently occuring number of time sequences go above their starting integer
+# 2) Most frequently occurring number of time sequences go above their starting integer
 
-max_count <- function(seq,start) {
-  value_greater_than_start <- FALSE
-  count <- 0
-  
-  for (value in seq) {
-    if(value > start) {
-      value_greater_than_start <- TRUE
-      count <- count + 1
-    }
-    if(value == 1) {
-      break
-    }
-  }
-  return(count)
+max_count <- function(seq, start) {
+  sum(seq > start)
 }
 
 count_df <- backtracks_df %>%
-  mutate(count = pmap_int(list(seq, start), max_count))
+  mutate(count=pmap_int(list(seq,start),max_count))
 
-my_mode <- function(x) {
-  unique_x <- unique(x)
-  tabulate_x <- tabulate(match(x, unique_x))
-  unique_x[tabulate_x == max(tabulate_x)]
+mode_backtrack <- count_df %>%
+  group_by(count) %>%
+  summarise(seq_count = n()) %>%
+  filter(seq_count == max(seq_count)) %>%
+  pull(count)
+
+mode_backtrack
+
+
+# 3) Maximum value reached after the first backtrack
+
+max_val_reached <- function(seq, start) {
+  value_less_than_start <- FALSE
+  max_val <- 0
+  
+  for (value in seq) {
+    if (value < start) {
+      value_less_than_start <- TRUE
+    }
+    if (value_less_than_start & value > start) {
+      max_val <- max(max_val,value)
+    }
+    if (value == 1) {
+      break
+    }
+  }
+  return(max_val)
 }
 
-mode_backtrack <- my_mode(count_df$count)
+max_after_backtrack <- backtracks_df %>%
+  mutate(max_val_reached = pmap_int(list(seq, start), max_val_reached))
 
-print(mode_backtrack)
+max_after_backtrack
+
+
+# 4) Frequency counts for even and odd backtracking integers
+
+even_odd_backtrack <- backtracks_df %>%
+  group_by(parity) %>%
+  count(parity)
+
+even_odd_backtrack
