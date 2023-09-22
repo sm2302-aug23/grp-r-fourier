bcc150047a8279e64f154d3c9503522195f235df
Fiqah-Alihan <20b2126@ubd.edu.bn>
2023-09-22 20:18:08 +0800

    Fiqah3-(Updated4)

--- 03-backtracking.R
+++ 03-backtracking.R
@@ -24,3 +24,35 @@ backtracks_df <- collatz_df %>%
   filter(pmap_lgl(list(seq, start), a_backtracking))
 
 print(backtracks_df)
+
+
+# 2) Most frequently occuring number of time sequences go above their starting integer
+
+max_count <- function(seq,start) {
+  value_greater_than_start <- FALSE
+  count <- 0
+  
+  for (value in seq) {
+    if(value > start) {
+      value_greater_than_start <- TRUE
+      count <- count + 1
+    }
+    if(value == 1) {
+      break
+    }
+  }
+  return(count)
+}
+
+count_df <- backtracks_df %>%
+  mutate(count = pmap_int(list(seq, start), max_count))
+
+my_mode <- function(x) {
+  unique_x <- unique(x)
+  tabulate_x <- tabulate(match(x, unique_x))
+  unique_x[tabulate_x == max(tabulate_x)]
+}
+
+mode_backtrack <- my_mode(count_df$count)
+
+print(mode_backtrack)
